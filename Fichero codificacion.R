36dae868714d951fcc91476aa522607ada116970
diegomochales2020 <diegomochales@usal.es>
2024-05-14 12:44:29 +0200

    Subida de fichero para futura codificación

---
+++ Fichero codificacion.R
@@ -0,0 +1,10 @@
+# Fichero trabajo final Data Science
+
+# Diego Mochales, David Barreales, Emilio Portela, Nayeli Paucart
+
+
+# Librerias
+library(tidyverse)
+library(dbplyr)
+library(ggplot2)
+library(ggrepel)
